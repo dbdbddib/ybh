c2e1488836a144cdd0794f3ac10f7478ed4ded30
dbdbddib <qudgns4475@gmail.com>
2024-10-07 19:15:17 +0900

    start
    start

---
+++ ybhprj/src/main/java/com/ybh/ybhprj/YbhprjApplication.java
@@ -0,0 +1,13 @@
+package com.ybh.ybhprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbhprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YbhprjApplication.class, args);
+	}
+
+}
